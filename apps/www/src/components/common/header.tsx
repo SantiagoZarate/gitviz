d3e0e58119ae717bdd8650f7238fa5694deedff8
SantiagoZarate <santiago2001zarate@gmail.com>
2025-03-02 13:49:35 -0300

    style(web): add icon to header

--- apps/www/src/components/common/header.tsx
+++ apps/www/src/components/common/header.tsx
@@ -1,4 +1,5 @@
 import { useGitContext } from '@/context/global-context';
+import { BranchIcon } from '../icons/branch-icon';
 
 export function Header() {
 	const { title, branch, contributors } = useGitContext();
@@ -9,7 +10,10 @@ export function Header() {
 		<header className='flex flex-col gap-2'>
 			<section className='flex items-center gap-2'>
 				<h1>{title}</h1>
-				<p className='text-sm'>{branch}</p>
+				<div className='flex items-center gap-1 rounded-full border px-2 py-1'>
+					<BranchIcon />
+					<p className='text-sm'>{branch}</p>
+				</div>
 			</section>
 			<p>Visualize git contributors on a repo</p>
 			<p>{totalLoC} Total lines of code</p>
---
+++ apps/www/src/components/icons/branch-icon.tsx
@@ -0,0 +1,21 @@
+export function BranchIcon() {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width={20}
+			height={20}
+			viewBox='0 0 24 24'
+		>
+			<g
+				fill='none'
+				stroke='currentColor'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				strokeWidth={1.5}
+			>
+				<path d='M6 15a3 3 0 1 1 0 6a3 3 0 0 1 0-6' />
+				<path d='M18 9a3 3 0 1 1 0-6a3 3 0 0 1 0 6m0 0a9 9 0 0 1-9 9m-3-3V3' />
+			</g>
+		</svg>
+	);
+}
